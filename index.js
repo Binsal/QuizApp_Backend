ef86a9f76be7ea4873a98a73220eb2547f9162d5
Binsal <96328531+Binsal@users.noreply.github.com>
2023-10-30 13:20:32 +0530

    added router

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const quizzes = require("./db/quizzes");
+const quizRouter = require("./router/quiz.router");
 
 const port=3000;
 
 app.use(cors());
 
+app.use(express.json());
+
 app.get("/",(req,res)=>{
     res.send("Hello");
 })
 
-app.get("/quiz",(req,res)=>{
-    res.send(quizzes.data);
-})
+app.use("/quiz",quizRouter);
 
 app.listen(process.env.PORT || port , ()=>{
     console.log("server is running");
