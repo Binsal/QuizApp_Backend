08d9563e2f350bc81b376f68e97af52789e6eb11
Binsal <96328531+Binsal@users.noreply.github.com>
2023-12-22 21:40:18 +0530

    testing on differennt dataset

--- db/quizzes.js
+++ db/quizzes.js
@@ -203,53 +203,53 @@ const quizzes = {
         // 1st Quiz Card --> will have title, category, description, quiz,
         {
             id: uuid(),
-            category: "mathematics",
+            category: "General Knowledge",
             image: "https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcHgxMzcwMjE4LWltYWdlLWt3eXFoYTRxLmpwZw.jpg",
-            title: "Mathematics",
+            title: "General Knowledge",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique quis nunc eget rutrum",
             quiz: [
                 //1st question and options
                 {
                     id: uuid(),
-                    question: "Mohan has some cocks and dogs. If the number of their heads is 108 and the number of their legs is 310, then find the number of cocks.",
+                    question: "What is the largest planet in our solar system?",
                                         options: [
-                                            { id: uuid(), option: "60", isCorrect: false },
-                                            { id: uuid(), option: "61", isCorrect: true },
-                                            { id: uuid(), option: "62", isCorrect: false },
-                                            { id: uuid(), option: "63", isCorrect: false }
+                                            { id: uuid(), option: "Venus", isCorrect: false },
+                                            { id: uuid(), option: "Mars", isCorrect: false },
+                                            { id: uuid(), option: "Jupiter", isCorrect: true },
+                                            { id: uuid(), option: "Saturn", isCorrect: false }
                                         ]
                 },
                  //2nd question and options
                 {
                     id: uuid(),
-                    question: "In a college, 65% of the students played cricket, 40% of the students played tennis and 25% of the students played both cricket and tennis.What percentage of the students played neither cricket nor tennis?",
+                    question: "Which of the following elements is a noble gas?",
                                         options: [
-                                            { id: uuid(), option: "20%", isCorrect: true },
-                                            { id: uuid(), option: "35%", isCorrect: false },
-                                            { id: uuid(), option: "10%", isCorrect: false },
-                                            { id: uuid(), option: "40%", isCorrect: false }
+                                            { id: uuid(), option: "Oxygen", isCorrect: false },
+                                            { id: uuid(), option: "Hydrogen", isCorrect: false },
+                                            { id: uuid(), option: "Helium", isCorrect: true },
+                                            { id: uuid(), option: "Carbon", isCorrect: false }
                                         ]
                 },
                  //3rd question and options
                 {
                     id: uuid(),
-                    question: "Find the square root of the perfect square made by multiplying 4050 with a least positive integer.",
+                    question: "Who wrote the play Romeo and Juliet?",
                                         options: [
-                                            { id: uuid(), option: "80", isCorrect: false },
-                                            { id: uuid(), option: "90", isCorrect: true },
-                                            { id: uuid(), option: "85", isCorrect: false },
-                                            { id: uuid(), option: "95", isCorrect: false }
+                                            { id: uuid(), option: "Charles Dickens", isCorrect: false },
+                                            { id: uuid(), option: "William Shakespeare", isCorrect: true },
+                                            { id: uuid(), option: "Jane Austen", isCorrect:false },
+                                            { id: uuid(), option: "Mark Twain", isCorrect: false }
                                         ]
                 },
                  //4th question and options
                 {
                     id: uuid(),
-                    question: "A monkey climbs a 12 meters high slippery pillar. In his first minute, he climbs 2 meters and in the next minute, he slips one meter down. In this way, how much time will he take to reach the top of the pillar?",
+                    question: "Which gas do plants absorb from the atmosphere during photosynthesis?",
                                         options: [
-                                            { id: uuid(), option: "10 minutes", isCorrect:false  },
-                                            { id: uuid(), option: "21 minutes", isCorrect: true },
-                                            { id: uuid(), option: "12 minutes", isCorrect: false },
-                                            { id: uuid(), option: "11 minutes", isCorrect: false }
+                                            { id: uuid(), option: "Nitrogen", isCorrect: false },
+                                            { id: uuid(), option: "Oxygen", isCorrect: false },
+                                            { id: uuid(), option: "Carbon dioxide", isCorrect: true },
+                                            { id: uuid(), option: "Hydrogen", isCorrect: false }
                                         ]
                 },
             ]
