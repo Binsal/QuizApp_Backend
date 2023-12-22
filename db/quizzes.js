0e20159de50cbd32bd1188d45f356eece3a209da
Binsal <96328531+Binsal@users.noreply.github.com>
2023-12-22 21:33:06 +0530

    testing on differennt dataset

--- db/quizzes.js
+++ db/quizzes.js
@@ -257,50 +257,50 @@ const quizzes = {
         //2nd quiz
         {
             id: uuid(),
-            category: "dc",
-            image: "https://media.proprofs.com/images/QM/user_images/2503852/New%20Project%20(56)(110).jpg",
-            title: "DC",
+            category: "science",
+            image: "https://img.freepik.com/premium-photo/chemical-test-tubes-with-colorful-liquids_798986-561.jpg",
+            title: "science",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique quis nunc eget rutrum",
             quiz: [
                 {
                     id: uuid(),
-                    question: "What was Wonder Woman originally named?",
+                    question: "What is the chemical symbol for gold?",
                     options: [
-                        { id: uuid(), option: "Anita", isCorrect: false },
-                        { id: uuid(), option: "Suprema", isCorrect: true },
-                        { id: uuid(), option: "Mishita", isCorrect: false },
-                        { id: uuid(), option: "None", isCorrect: false }
+                        { id: uuid(), option: "Ag", isCorrect: false },
+                        { id: uuid(), option: "Go", isCorrect: false },
+                        { id: uuid(), option: "Au", isCorrect: true },
+                        { id: uuid(), option: "Ag", isCorrect: false }
                     ]
                 },
                 {
                     id: uuid(),
-                    question: "Which supervillain was once the Iranian Ambassador to the U.N.?",
-                    options: [
-                        { id: uuid(), option: "Wonder Man", isCorrect: false },
-                        { id: uuid(), option: "Superman", isCorrect: false },
-                        { id: uuid(), option: "The Joker", isCorrect: true },
-                        { id: uuid(), option: "Batman", isCorrect: false }
-                    ]
+                    question: "What is the unit of electric current?",
+                                        options: [
+                                            { id: uuid(), option: "Volt", isCorrect: false },
+                                            { id: uuid(), option: "Watt", isCorrect: false },
+                                            { id: uuid(), option: "Ohm", isCorrect: false },
+                                            { id: uuid(), option: "Ampere", isCorrect: true }
+                                        ]
                 },
                 {
                     id: uuid(),
-                    question: "What is batman's favorite food?",
-                    options: [
-                        { id: uuid(), option: "Chicken Salad", isCorrect: false },
-                        { id: uuid(), option: "Turkey Roast", isCorrect: false },
-                        { id: uuid(), option: "Goat Ribbs", isCorrect: false },
-                        { id: uuid(), option: "Mulligatawny soup", isCorrect: true }
-                    ]
+                    question: "Which gas do plants absorb from the atmosphere during photosynthesis?",
+                                        options: [
+                                            { id: uuid(), option: "Nitrogen", isCorrect: false },
+                                            { id: uuid(), option: "Oxygen", isCorrect: false },
+                                            { id: uuid(), option: "Carbon dioxide", isCorrect: true },
+                                            { id: uuid(), option: "Hydrogen", isCorrect: false }
+                                        ]
                 },
                 {
                     id: uuid(),
-                    question: "In how many seconds can Green Arrow shoot and reload?",
-                    options: [
-                        { id: uuid(), option: "Two and a half seconds", isCorrect: true },
-                        { id: uuid(), option: "Two seconds", isCorrect: false },
-                        { id: uuid(), option: "One seconds", isCorrect: false },
-                        { id: uuid(), option: "One and a half seconds", isCorrect: false }
-                    ]
+                    question: "What is the study of the Earth's atmosphere, weather, and climate called?",
+                                        options: [
+                                            { id: uuid(), option: "Meteorology", isCorrect: true },
+                                            { id: uuid(), option: "Astronomy", isCorrect: false },
+                                            { id: uuid(), option: "Oceanography", isCorrect: false },
+                                            { id: uuid(), option: "Geology", isCorrect: false }
+                                        ]
                 },
             ]
         },
